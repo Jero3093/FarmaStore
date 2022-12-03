05f5cf7ee5cca3e98a3f618374276ba5e2c3e751
Jero3093 <101958629+Jero3093@users.noreply.github.com>
2022-12-03 15:30:03 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+/*--Menu Desplegable--*/
+function mostrar_menu(){
+    document.getElementById('contenedor-menu').style.display = 'block'
+}
+
+function ocultar_menu(){
+    document.getElementById('contenedor-menu').style.display = 'none'
+}
+/*-----------------------------*/
